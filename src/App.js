aba90f09e32d5b4d732c5ba7530aed02577ec1c6
Rodrigo Goncalves <rodrigo.rebes@gmail.com>
2021-08-24 19:20:26 -0300

    Implementado a requisição para API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import axios from 'axios';
+import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
+import logoCadastro from './assets/planilha-de-ligacoes.png';
 
 function App() {
+
+  const baseUrl = "https://localhost:44302/api/alunos";
+  const [data, setData] = useState([]);
+
+  const pedidoGet = async () => {
+    await axios.get(baseUrl)
+      .then(response => {
+        setData(response.data);
+      }).catch(error => {
+        console.log(error);
+      })
+  }
+  debugger;
+
+  useEffect(() => {
+    pedidoGet();
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <br />
+      <h3>Cadastro de Alunos</h3>
+      <header>
+        {/* <img src={logoCadastro} alt='Cadastro' /> */}
+        <button className="btn btn-success">Incluir Novo Aluno</button>
       </header>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Idade</th>
+            <th>Operação</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(aluno => (
+            <tr key={aluno.id}>
+              <td>aluno.id</td>
+              <td>aluno.nome</td>
+              <td>aluno.email</td>
+              <td>aluno.idade</td>
+              <td>
+                <button className="btn btn-primary">Editar</button> {" "}
+                <button className="btn btn-danger">Excluir</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
